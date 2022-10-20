be0896b5178cab7c7dbbecfd6cdecc8f37c8e75b
agarmir0920 <tsarevantuan@yandex.ru>
2022-10-20 19:27:11 +0300

    Started writting slime class implementation

--- src/slime.cpp
+++ src/slime.cpp
@@ -6,9 +6,17 @@ void Slime::update(const size_t ms)
 bool Slime::getIntersection(Point &pos, RGBColor &color, const Ray &ray)
 {
     if (cover.isIntersected(ray))
-    {}
+        return false;
 
-    return false;
+    bool isIntersected = false;
+    Point p;
+
+    for (auto it: faces)
+    {
+        // ...
+    }
+
+    return isIntersected;
 }
 
 bool Slime::getIntersectedPlane(shared<PlaneFace> &face, const Ray &ray)
